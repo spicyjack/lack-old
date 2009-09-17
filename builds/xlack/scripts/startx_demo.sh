db8d077d9c545a72393e3bc4520546cb180abc7d
Brian Manning <elspicyjack at gmail dot com>
2009-09-17 01:44:52 -0700

    Added missing files to xlack lenny perl recipe; Added test for 'xterm'

--- builds/xlack/scripts/startx_demo.sh
+++ builds/xlack/scripts/startx_demo.sh
@@ -8,7 +8,8 @@ STARTX="/usr/bin/startx"
 # see if we even want to run X
 if [ $(/bin/grep -c nox /proc/cmdline) -eq 0 ]; then
     # yep, run X; now do we want a normal or debug session?
-    if [ $(/bin/grep -c DEBUG /proc/cmdline) -gt 0 ]; then
+    # check for either debugging or explicit xterm call
+    if [ $(/bin/egrep -c "DEBUG|xterm" /proc/cmdline) -gt 0 ]; then
         # debug session
         cat /home/demo/xsession | sed "s/^#\(exec xterm.*\)$/\1/" \
             > $HOME_DIR/.xsession
