af1a908b0dd3babe07d05339f2d3d61bb7de15f8
Brian Manning <brian at portaboom dot com>
2010-12-03 19:35:54 -0800

    added more script documentation, including a new doc tag, REQ for 'required'

--- scripts/common_release_functions.sh
+++ scripts/common_release_functions.sh
@@ -7,8 +7,10 @@
 CAT=$(which cat)
 SED=$(which sed)
 
-## FUNC: check_filelist_envvar - Check the $FILELIST environment variable
-## DESC: exits the script with an error if the filelist variable is not set
+## FUNC: check_filelist_envvar
+## REQ:  $FILELIST - the filename of the output filelist file
+## DESC: Check the $FILELIST environment variable.  Exits the script 
+## DESC: with an error if the filelist variable is not set
 function check_filelist_envvar {
     if [ -z $FILELIST ]; then
         echo "ERROR: FILELIST variable empty!"
@@ -16,8 +18,10 @@ function check_filelist_envvar {
     fi # if [ -z $FILELIST ]; then
 } # function check_filelist_envvar
 
-## FUNC: check_project_list_envvar - Check for the $PROJECT_LIST variable
-## DESC: exits the script with an error if the project list variable is not set
+## FUNC: check_project_list_envvar
+## ARG:  None
+## DESC: Check for the $PROJECT_LIST variable.  Exits the script 
+## DESC: with an error if the project list variable is not set
 function check_project_list_envvar {
     if [ -z $PROJECT_LIST ]; then
         echo "ERROR: PROJECT_LIST variable empty!"
@@ -26,6 +30,7 @@ function check_project_list_envvar {
 } # function check_project_list_envvar
 
 ## FUNC: check_temp_dir_envvar - Check the $TEMP_DIR environment variable
+## ARG:  None
 ## DESC: exits the script with an error if the temporary (working) directory
 ## DESC: variable is not set
 function check_temp_dir_envvar {
@@ -35,10 +40,12 @@ function check_temp_dir_envvar {
     fi # if [ -z $FILELIST ]; then
 } # function check_temp_dir_envvar
 
-## FUNC: check_base_file_exists - Check for the $PROJECT_NAME.base.txt
-## DESC: Exits the script with an error if the project base file is missing.
+## FUNC: check_base_file_exists
+## ARG:  None
+## DESC: Check for the $PROJECT_NAME.base.txt.  Exits the script 
+## DESC: with an error if the project base file is missing.  
 ## DESC: The base file is a filelist with a specific set of files to be
-## DESC: included with this project
+## DESC: included with this project.
 function check_base_file_exists {
     if [ ! -e $PROJECT_DIR/${PROJECT_NAME}.base.txt ]; then
         echo "ERROR: ${PROJECT_DIR}/${PROJECT_NAME}.base.txt file missing"
@@ -46,10 +53,11 @@ function check_base_file_exists {
     fi # if [ $PROJECT_DIR/${PROJECT_NAME}.base.txt ]
 } # function check_base_file_exists
 
-## FUNC: create_initramfs_filelist - Creates the initramfs filelist
-## DESC: Writes the initramfs filelist to the $TEMP_DIR
-## DESC: Requires that the $PROJECT_DIR and $PROJECT_NAME environment
-## DESC: variables be set prior to calling this function
+## FUNC: create_initramfs_filelist
+## ARG:  None
+## DESC: Creates the initramfs filelist.  Writes the initramfs filelist to
+## DESC: the $TEMP_DIR.  Requires that the $PROJECT_DIR and $PROJECT_NAME
+## DESC: environment variables be set prior to calling this function
 function create_initramfs_filelist {
     if [ -e $PROJECT_DIR/kernel_configs/linux-image-$1.txt ]; then
         cat $PROJECT_DIR/${PROJECT_NAME}.base.txt \
@@ -62,31 +70,36 @@ function create_initramfs_filelist {
     fi
 } # function create_initramfs_filelist
 
-## FUNC: create_hostname_file - Create the hostname file in $TEMP_DIR
+## FUNC: create_hostname_file
+## ARG:  None
+## DESC: Create the hostname file in $TEMP_DIR
 function create_hostname_file {
     echo $PROJECT_NAME > $TEMP_DIR/hostname
 } # function check_filelist_envvar
 
-## FUNC: create_standalone_init_script - Create the /init script
+## FUNC: create_standalone_init_script
+## ARG:  None
 ## DESC: Creates the standalone version of the /init script that the Linux
 ## DESC: kernel runs once it loads the initramfs image into RAM.
-## DESC: Performs a bunch of variable substitutions when it copies the file to
-## DESC: $TEMP_DIR
+## DESC: Performs a bunch of text substitutions via sed when the file
+## DESC: is copied to $TEMP_DIR
 function create_standalone_init_script {
     _create_init_script _init.sh
 } # function create_standalone_init_script
 
 ## FUNC: create_initramfs_init_script - Create the /init script
+## ARG:  None
 ## DESC: Creates the initramfs version of the /init script that the Linux
 ## DESC: kernel runs once it loads the initramfs image into RAM.
 ## DESC: This script calls Busybox /sbin/init at the tail end of the init
-## DESC: process.  Performs a bunch of variable substitutions when it copies
-## DESC: the file to $TEMP_DIR.
+## DESC: process.  Performs a bunch of text substitutions via sed 
+## DESC: when it copies the file to $TEMP_DIR.
 function create_initramfs_init_script {
     _create_init_script _initramfs_init.sh
 } # function create_initramfs_init_script
 
-## FUNC: _create_standalone_init_script - Actually create the /init script
+## FUNC: _create_standalone_init_script
+## ARG:  path to the source init script
 ## DESC: Called by one of the other init creators.  Does the actual work of
 ## DESC: creating the script and processing any variable substitutions 
 function _create_init_script {
@@ -103,9 +116,13 @@ function _create_init_script {
     echo "file /init /${TEMP_DIR}/init.sh 0755 0 0" >> $TEMP_DIR/$FILELIST
 } # function create_standalone_init_script
 
-## FUNC: copy_ssl_pems - Copy the *.pem files needed for SSL usage
+## FUNC: copy_ssl_pems
+## ARG:  None
+## DESC: Copy the *.pem files needed for SSL usage
 ## DESC: Exits if it can't find the correct files in the ~/stuff_tars
-## DESC: directory.  Requires that $PROJECT_NAME and $TEMP_DIR be set
+## DESC: directory.  
+## REQ:  $PROJECT_NAME - name of the project, also usually the hostname
+## REQ:  $TEMP_DIR - working directory
 function copy_ssl_pem_files {
     if [ -e ~/stuff_tars/${PROJECT_NAME}*.key.pem.nopass ]; then
         cp ~/stuff_tars/${PROJECT_NAME}*.pem* $TEMP_DIR
@@ -115,8 +132,11 @@ function copy_ssl_pem_files {
     fi # if [ -e ~/stuff_tars/pigwidgeon.key.pem.nopass ]
 } # function copy_ssl_pem_files
 
-## FUNC: copy_busybox_binary - Copies the Busybox binary to $TEMP_DIR
-## DESC: Exits if it can't find the Busybox binary.  Requires $TEMP_DIR be set
+## FUNC: copy_busybox_binary
+## ARG:  None
+## REQ:  $TEMP_DIR
+## DESC: Copies the Busybox binary to $TEMP_DIR
+## DESC: Exits if it can't find the Busybox binary.
 function copy_busybox_binary {
     if [ -e ~/busybox-*-x86 ]; then
         cp ~/busybox-*-x86 $TEMP_DIR
