665b0fce16a792e6bdaf7d33603ea266547eac2d
Brian Manning <brian@xaoc.org>
2013-04-23 14:02:57 -0700

    removed old unneeded files

--- scripts/lint_recipes.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-# $Id: lint_recipes.pl,v 1.1 2009-01-17 03:57:36 brian Exp $
-# Copyright (c)2007 by Brian Manning <brian at portaboom dot com>
-
-# script to 'lint' recipie files; 'lint' in this case means check for metadata
-# in the comments and report what is and is not found.  These are meant to be
-# suggestions only, you don't *have* to have the metadata in the comments 
