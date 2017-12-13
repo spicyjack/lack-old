f88a80b0a259e0431c3ee0a144825da6f0c176d8
Brian Manning <brian@xaoc.org>
2017-12-12 18:25:51 -0800

    scripts/keystore/gnupg-batch.sh: fixed whitespace/tabs/comments

--- scripts/keystore/gnupg-batch.sh
+++ scripts/keystore/gnupg-batch.sh
@@ -40,12 +40,12 @@ CHALLENGE=0
 KEY_OUT_DIR="/home/brian/keys"
 # path to diceparse.pl; change with: --dicepath
 DICEPATH="/home/brian/src/perl-hg/"
-# generate a diceware phrase consisting of this many words (rolls); 
+# generate a diceware phrase consisting of this many words (rolls);
 # change with: --dicesize
 DICESIZE="5"
 # path to word list; change with: --wordlist
 WORDLIST="/home/brian/diceware.en.txt"
-# generate this many gpg keys; 
+# generate this many gpg keys;
 # change with: --count, default = 0 (generate keys until infinity,
 # or until you run out of hard drive space)
 KEY_COUNT=0
@@ -67,18 +67,18 @@ function cmd_status () {
     STATUS=$2
     if [ $STATUS -ne 0 ]; then
         echo "Command '${COMMAND}' failed with status code: ${STATUS}"
-        exit 1 
+        exit 1
     fi
-} # cmd_status
+}
 
 function get_dice_passphrase () {
     local KEY_SIZE=$1
     PASSPHRASE=$(perl ${DICEPATH}/diceparse.pl -r ${KEY_SIZE} -l ${WORDLIST})
     cmd_status "diceparse.pl" $?
-} # get_dice_passphrase
+}
 
 function temp_key_move () {
-	# move the generated keys from the temp directory to the --output directory
+    # move the generated keys from the temp directory to the --output directory
 
     # exit if there's no keylist file (empty directory?)
     if [ ! -e $OUTDIR/keylist.txt ]; then return; fi
@@ -108,23 +108,23 @@ function temp_key_move () {
         if [ -e $OUTDIR/challenge.txt ]; then exit 1; fi
     fi # if [ $CHALLENGE -gt 0 ]
 
-} # temp_key_move
+}
 
 function rename_keys () {
-	# rename the keystore files to the name of their key ID
+    # rename the keystore files to the name of their key ID
     if [ $QUIET -gt 0 ]; then
-		echo "Renaming keystore.[sec|pub] to ${KEY_ID}.[sec|pub]"
-	fi
-	$MV $OUTDIR/keystore.sec $OUTDIR/$KEY_ID.sec
+        echo "Renaming keystore.[sec|pub] to ${KEY_ID}.[sec|pub]"
+    fi
+    $MV $OUTDIR/keystore.sec $OUTDIR/$KEY_ID.sec
     cmd_status "(secret key) mv" $?
     $MV $OUTDIR/keystore.pub $OUTDIR/$KEY_ID.pub
     cmd_status "(public key) mv" $?
-} # rename_keys
+}
 
 function show_help () {
-    cat <<-EOF 
+    cat <<-EOF
 
-    ${SCRIPTNAME} [options] 
+    ${SCRIPTNAME} [options]
 
     SCRIPT OPTIONS
     -h|--help      Show this help output
@@ -135,7 +135,7 @@ function show_help () {
     --overwrite    Overwrite duplicate keys; multiple entries will exist in
                    the keylist file
     -c|--count     Create this many keys only
-    -t|--tempdir   Write 'X' number of keys to this directory, then move the 
+    -t|--tempdir   Write 'X' number of keys to this directory, then move the
                    keys to a new directory in the output directory (saves on
                    disk writes; tempdir should be a tmpfs filesystem like
                    /dev/shm in Linux)
@@ -147,7 +147,7 @@ function show_help () {
 
 EOF
     exit 0
-} # show_help
+}
 
 function show_examples () {
     cat <<-EOE
@@ -158,10 +158,10 @@ function show_examples () {
 
 EOE
 
-} # show_examples
+}
 
 ### BEGIN SCRIPT ###
-# BSD's getopt is simpler than the GNU getopt; we need to detect it 
+# BSD's getopt is simpler than the GNU getopt; we need to detect it
 OSDETECT=$($UNAME -s)
 if [ $OSDETECT == "Darwin" ]; then
     # this is the BSD part
@@ -205,37 +205,37 @@ while true ; do
         -p|--dicepath) # path to the diceparse.pl script
             DICEPATH=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --dicepath
         -s|--dicesize) # size of the password to generate using diceparse.pl
             DICESIZE=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --dicepath
         -o|--output) # final output directory for keys and keylist files
             KEY_OUT_DIR=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --output
         -c|--count) # how many keys to generate
             KEY_COUNT=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --count
         -t|--tempdir) # temporary directory for generating keys
             KEY_TEMP_DIR=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --tempdir
         -n|--tempnum) # number of keys to generate before moving them all to
-			# directory specified in --output (KEY_OUT_DIR)
+            # directory specified in --output (KEY_OUT_DIR)
             KEY_TEMP_NUM=$2
             shift 2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             ;; # --tempnum
         --overwrite) # overwrite existing keys (script exits if not set)
             OVERWRITE_KEYS=1
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             shift
             ;; # --overwrite        
         -q|--quiet) # don't output anything (unless there's an error)
@@ -247,12 +247,12 @@ while true ; do
             ;; # --quiet
         -v|--verbose) # output pretty messages
             QUIET=2
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             shift
             ;; # --verbose
         -x|--challenge) # output pretty messages
             CHALLENGE=1
-			ERRORLOOP=$(($ERRORLOOP - 1))
+            ERRORLOOP=$(($ERRORLOOP - 1))
             shift
             ;; # --verbose
         --) shift; break
@@ -265,14 +265,13 @@ while true ; do
         echo "Maybe you have a getopt option with no branch?" >&2
         exit 1
     fi # if [ $ERROR_LOOP -gt 3 ];
-    
 done
 
 # tell the keys where to go
 if [ $KEY_TEMP_DIR ]; then
-	OUTDIR=$KEY_TEMP_DIR
-else 
-	OUTDIR=$KEY_OUT_DIR
+    OUTDIR=$KEY_TEMP_DIR
+else
+    OUTDIR=$KEY_OUT_DIR
 fi
 
 if [ $QUIET -gt 0 ]; then echo "- Writing keys to '${OUTDIR}'"; fi
@@ -295,14 +294,14 @@ if [ $QUIET -gt 1 ]; then echo "- sedded output directory ${SED_OUTDIR}"; fi
 # some counters
 KEY_COUNTER=0
 # run gnupg multiple times generating keys
-while $TRUE; do 
+while $TRUE; do
     # generate a shorter challenge passphrase
     # a second file will be created that lists challenge words and key ID's
     get_dice_passphrase 3
     CHALLENGE_PASSPHRASE=$PASSPHRASE
 
     # grab a GPG key password from diceparse; sets $PASSPHRASE
-    get_dice_passphrase 5 
+    get_dice_passphrase 5
 
     # make a new file from the template
     cat batch.gnupg.template | sed "s/#PASSPHRASE#/${PASSPHRASE}/" \
@@ -319,20 +318,20 @@ while $TRUE; do
         head -n 3 | tail -n 1 | \
         sed -e 's/sec   1024D\///' -e 's/ .*$//' )
         #sed '{s/sec   1024D\///; s/ .*$//}' )
-	if [ $QUIET -gt 1 ]; then echo "key ID is: ${KEY_ID}"; fi	
+    if [ $QUIET -gt 1 ]; then echo "key ID is: ${KEY_ID}"; fi
     # rename them
     if [ ! -e $OUTDIR/$KEY_ID.sec -a ! -e $OUTDIR/$KEY_ID.pub ]; then
-		rename_keys
-    else 
-		if [ $OVERWRITE ]; then
-			if [ $QUIET -gt 0 ]; then
-				echo "WARNING: duplicate key ${KEY_ID} was just overwritten"
-			fi
-			rename_keys
-		else 
-	        echo "ERROR: key ${KEY_ID} exists! Will not overwrite key!"
-   			exit 1
-		fi # if [ $OVERWRITE ];
+        rename_keys
+    else
+        if [ $OVERWRITE ]; then
+            if [ $QUIET -gt 0 ]; then
+                echo "WARNING: duplicate key ${KEY_ID} was just overwritten"
+            fi
+            rename_keys
+        else
+            echo "ERROR: key ${KEY_ID} exists! Will not overwrite key!"
+               exit 1
+        fi # if [ $OVERWRITE ];
     fi # if [ ! -e $OUTDIR/$KEY_ID.sec -a ! -e $OUTDIR/$KEY_ID.pub ];
     NOW=$(date | tr -d '\n')
     if [ $CHALLENGE -gt 0 ]; then
@@ -340,23 +339,23 @@ while $TRUE; do
     fi # if [ $CHALLENGE -gt 0 ]
     echo "${NOW} ${KEY_ID} ${PASSPHRASE}" >> $OUTDIR/keylist.txt
     if [ $QUIET -gt 0 ]; then echo "added ${KEY_ID} to keylist.txt"; fi
-	# increment the counter
-	KEY_COUNTER=$(($KEY_COUNTER + 1))
-	# see if we've hit the temp counter
-	MODULUS=$(($KEY_COUNTER % $KEY_TEMP_NUM))
-	if [ $QUIET -gt 1 ]; then echo "- modulus was $MODULUS"; fi
-	#if [ "$KEY_TEMP_DIR" -a $MODULUS -eq 0 ]; then
-	if [ $MODULUS -eq 0 ]; then
-		if [ $KEY_TEMP_DIR ]; then
-			temp_key_move
-		fi # if [ -n $KEY_TEMP_DIR ];
-	fi # if [ -n $KEY_TEMP_DIR -a $MODULUS -eq 0 ];
-	if [ $KEY_COUNTER -eq $KEY_COUNT ]; then
-		if [ $KEY_TEMP_DIR ]; then
-			temp_key_move
-		fi # if [ -n $KEY_TEMP_DIR ];
-		break
-	fi # if [ $KEY_COUNTER -eq $KEY_COUNT ];
+    # increment the counter
+    KEY_COUNTER=$(($KEY_COUNTER + 1))
+    # see if we've hit the temp counter
+    MODULUS=$(($KEY_COUNTER % $KEY_TEMP_NUM))
+    if [ $QUIET -gt 1 ]; then echo "- modulus was $MODULUS"; fi
+    #if [ "$KEY_TEMP_DIR" -a $MODULUS -eq 0 ]; then
+    if [ $MODULUS -eq 0 ]; then
+        if [ $KEY_TEMP_DIR ]; then
+            temp_key_move
+        fi # if [ -n $KEY_TEMP_DIR ];
+    fi # if [ -n $KEY_TEMP_DIR -a $MODULUS -eq 0 ];
+    if [ $KEY_COUNTER -eq $KEY_COUNT ]; then
+        if [ $KEY_TEMP_DIR ]; then
+            temp_key_move
+        fi # if [ -n $KEY_TEMP_DIR ];
+        break
+    fi # if [ $KEY_COUNTER -eq $KEY_COUNT ];
 done
-# vi: set sw=4 ts=4: 
+# vi: set sw=4 ts=4:
 # end of line
