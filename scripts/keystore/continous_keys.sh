e2044e29e50499d02609800b4103892bd5e93d89
Brian Manning <brian@xaoc.org>
2017-12-12 19:13:55 -0800

    continous_keys.sh: updated paths, fixed whitespace

--- scripts/keystore/continous_keys.sh
+++ scripts/keystore/continous_keys.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# FIXME 
+# FIXME
 # - grab stats from each gnupg key creation, so that you can come up with
 # performance metrics
 
-while /bin/true; 
-do 
+while /bin/true;
+do
     OUTDATE=$(date +%d%b%Y)
-    echo "Creating new GPG key folder in /dev/shm/${OUTDATE}"; 
+    echo "Creating new GPG key folder in /dev/shm/${OUTDATE}";
     time sh ~/src/lack.hg/scripts/keystore/gnupg-batch.sh
         --output /dev/shm/${OUTDATE} \
         --count 500 \
-        --dicepath ~/src/perl.hg/diceware \
-        --list ~/src/perl.hg/diceware/diceware.wordlist.asc
+        --dicepath ~/src/perl.git/diceware \
+        --list ~/src/perl.git/diceware/diceware.wordlist.asc
 done
